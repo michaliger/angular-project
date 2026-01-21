8843b98724f47925bbd54f05c9722b3617e0583d
michaliger <michal6761991@gmail.com>
2026-01-21 22:04:00 +0200

    add styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in'; // וודאי שהנתיב פה נכון
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'signin', component: SignInComponent },
+  { path: '', redirectTo: 'signin', pathMatch: 'full' } // זה יפתח את העמוד ישר בהתחלה
+];
--- src/app/sign-in/sign-in.ts
+++ src/app/sign-in/sign-in.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true, // הגדרה כרכיב עצמאי
-  imports: [CommonModule, ReactiveFormsModule], // ייבוא המודולים ישירות לכאן
+  imports: [CommonModule, ReactiveFormsModule,CommonModule, 
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule], // ייבוא המודולים ישירות לכאן
   templateUrl: './sign-in.html',
   styleUrls: ['./sign-in.css']
 })
